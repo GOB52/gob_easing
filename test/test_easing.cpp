0099c85f2b60593c5fe55a0eaf9031ef95303f79
GOB <goblin52@gmail.com>
2024-01-20 19:09:58 +0900

    Fixes compare fp

--- test/test_easing.cpp
+++ test/test_easing.cpp
@@ -3,14 +3,12 @@
 #include <gob_easing.hpp>
 #include <cmath>
 #include <limits>
-#include <random>
 
-auto rng = std::default_random_engine {};
 using namespace goblib;
 
+#if defined(GOBLIB_EASING_USING_OWN_MATH)
 namespace
 {
-std::uniform_real_distribution<> dist(0.0f, 10000.0f);
 
 template<typename T> constexpr T deg2rad(T deg)
 {
@@ -18,156 +16,179 @@ template<typename T> constexpr T deg2rad(T deg)
     return deg * (M_PI / T{180});
 }
 
-// Match to n decimal places
-template<unsigned int DP = 4> bool match_fp(const float x, const float y)
+// Approximately equal
+template<typename T> bool fp_eq(const T a, const T b)
 {
-    long int mp = std::pow(10L, DP);
-    return static_cast<long int>(std::round(x * mp)) == static_cast<long int>(std::round(y * mp));
+    static_assert(std::is_floating_point<T>::value, "T must be floating point");
+    return (fabs(a) != std::numeric_limits<T>::infinity() && fabs(b) != std::numeric_limits<T>::infinity()) ?
+            (fabs(a - b) <= T{0.00001} ? true :
+             fabs(a - b) <= ( (fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * std::numeric_limits<T>::epsilon()))
+            : true;
+}
+//
 }
 
-
-}//
-
-#if defined(GOBLIB_EASING_USING_OWN_MATH)
+// -------------------------------------
 // sqrt
+namespace
+{
+template<typename T> void test_sqrt()
+{
+    constexpr T table[] =  { 0.0, 0.5,      1.0, 2.0,      4.0f, std::numeric_limits<T>::infinity(), -0.0f };
+    constexpr T answer[] = { 0.0, 0.707107, 1.0, 1.414214, 2.0f, std::numeric_limits<T>::infinity(), -0.0f };
+    static_assert(sizeof(table)/sizeof(table[0]) == sizeof(answer)/sizeof(answer[0]), "Oops");
+
+    for(size_t i=0;i<sizeof(table)/sizeof(table[0]);++i)
+    {
+        auto s = std::sqrt(table[i]);
+        auto e = easing::math::sqrt(table[i]);
+        EXPECT_TRUE(fp_eq(s, e)) << table[i] << answer[i];
+    }
+    // for negative (Notice: NAN == NAN make false)
+    {
+        auto s = std::sqrt(-1.0f);
+        auto e = easing::math::sqrt(-1.0f);
+        EXPECT_FALSE(s == s);
+        EXPECT_FALSE(e == e);
+        EXPECT_FALSE(s == e);
+    }
+}
+//
+}
+
 TEST(own_math, sqrt)
 {
     // constexpr
-    constexpr double cd = easing::math::sqrt(2.0);
     constexpr float cf = easing::math::sqrt(2.0f);
-    EXPECT_FLOAT_EQ(cd, 1.414214);
-    EXPECT_FLOAT_EQ(cf, 1.414214f);
+    constexpr double cd = easing::math::sqrt(2.0);
+    constexpr long double cld = easing::math::sqrt(2.0L);
+    EXPECT_TRUE(fp_eq(std::sqrt(2.0f), cf));
+    EXPECT_TRUE(fp_eq(std::sqrt(2.0), cd));
+    EXPECT_TRUE(fp_eq(std::sqrt(2.0L), cld));
 
-    // double
-    {
-        constexpr double table[] =  { 0.0, 0.5,      1.0, 2.0,      4.0, std::numeric_limits<double>::infinity(), -0.0 };
-        constexpr double answer[] = { 0.0, 0.707107, 1.0, 1.414214, 2.0, std::numeric_limits<double>::infinity(), -0.0 };
-        static_assert(sizeof(table)/sizeof(table[0]) == sizeof(answer)/sizeof(answer[0]), "Oops");
-
-        for(size_t i=0;i<sizeof(table)/sizeof(table[0]);++i)
-        {
-            auto s = std::sqrt(table[i]);
-            auto e = easing::math::sqrt(table[i]);
-            EXPECT_FLOAT_EQ(s, e) << table[i] << answer[i];
-        }
-
-        // for negative (Notice: NAN == NAN make false)
-        {
-            auto s = std::sqrt(-1.0);
-            auto e = easing::math::sqrt(-1.0);
-            EXPECT_FALSE(s == s);
-            EXPECT_FALSE(e == e);
-            EXPECT_FALSE(s == e);
-        }
-    }
-
-    // float
-    {
-        constexpr float table[] =  { 0.0f, 0.5f,      1.0f, 2.0f,      4.0f, std::numeric_limits<float>::infinity(), -0.0f };
-        constexpr float answer[] = { 0.0f, 0.707107f, 1.0f, 1.414214f, 2.0f, std::numeric_limits<float>::infinity(), -0.0f };
-        static_assert(sizeof(table)/sizeof(table[0]) == sizeof(answer)/sizeof(answer[0]), "Oops");
-
-        for(size_t i=0;i<sizeof(table)/sizeof(table[0]);++i)
-        {
-            auto s = std::sqrt(table[i]);
-            auto e = easing::math::sqrt(table[i]);
-            EXPECT_FLOAT_EQ(s, e) << table[i] << answer[i];
-        }
-
-        // for negative (Notice: NAN == NAN make false)
-        {
-            auto s = std::sqrt(-1.0f);
-            auto e = easing::math::sqrt(-1.0f);
-            EXPECT_FALSE(s == s);
-            EXPECT_FALSE(e == e);
-            EXPECT_FALSE(s == e);
-        }
-    }
+    test_sqrt<float>();
+    test_sqrt<double>();
+    test_sqrt<long double>();
 }    
 
+// -------------------------------------
 // pow
+namespace
+{
+template<typename T> void test_pow()
+{
+    {
+        auto s = std::pow(T{2.0}, std::numeric_limits<T>::infinity());
+        constexpr auto e = easing::math::pow(T{2.0}, std::numeric_limits<T>::infinity());
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, T{2.0});
+        constexpr auto e = easing::math::pow(T{2.0}, T{2.0});
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, T{1.0});
+        constexpr auto e = easing::math::pow(T{2.0}, T{1.0});
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, T{0.5});
+        constexpr auto e = easing::math::pow(T{2.0}, T{0.5});
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, T{0.0});
+        constexpr auto e = easing::math::pow(T{2.0}, T{0.0});
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, T{-0.5});
+        constexpr auto e = easing::math::pow(T{2.0}, T{-0.5});
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, T{-1.0f});
+        constexpr auto e = easing::math::pow(T{2.0}, T{-1.0f});
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, T{-2.0});
+        constexpr auto e = easing::math::pow(T{2.0}, T{-2.0});
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+    {
+        auto s = std::pow(T{2.0}, -std::numeric_limits<T>::infinity());
+        constexpr auto e = easing::math::pow(T{2.0}, -std::numeric_limits<T>::infinity());
+        EXPECT_TRUE(fp_eq(s,e));
+    }
+}
+//
+}
+
 TEST(own_math, power)
 {
-    //
+    // contexpr
     {
-        constexpr double ce = easing::math::pow(2.0, 0.5);
         constexpr float  cf = easing::math::pow(2.0f, 0.5f);
-        EXPECT_FLOAT_EQ(ce, 1.414214);
-        EXPECT_FLOAT_EQ(cf, 1.414214f);
+        constexpr double cd = easing::math::pow(2.0, 0.5);
+        constexpr long double cld = easing::math::pow(2.0L, 0.5L);
+        EXPECT_TRUE(fp_eq(std::pow(2.0f, 0.5f), cf));
+        EXPECT_TRUE(fp_eq(std::pow(2.0,  0.5),  cd));
+        EXPECT_TRUE(fp_eq(std::pow(2.0L, 0.5L), cld));
     }
 
     // exp is integer
     {
         for(int i = 0;i < 32;++i)
         {
-            auto v = dist(rng);
-            EXPECT_FLOAT_EQ(std::pow(v, i), easing::math::pow(v, i)) << v << " | " << i;
+            float f = (float)i;
+            double d = (double)i;
+            long double ld = (long double)i;
+            EXPECT_TRUE(fp_eq((float)std::pow( f, i), easing::math::pow( f, i))) << f << " | " << i;
+            EXPECT_TRUE(fp_eq(std::pow( d, i), easing::math::pow( d, i))) << d << " | " << i;
+            EXPECT_TRUE(fp_eq(std::pow(ld, i), easing::math::pow(ld, i))) << ld << " | " << i;
         }
     }
-
-    // exp is floating point number
-    {
-        auto s = std::pow(2.0, std::numeric_limits<double>::infinity());
-        constexpr auto e = easing::math::pow(2.0, std::numeric_limits<double>::infinity());
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0,2.0);
-        constexpr auto e = easing::math::pow(2.0, 2.0);
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0,1.0);
-        constexpr auto e = easing::math::pow(2.0, 1.0);
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0,0.5);
-        constexpr auto e = easing::math::pow(2.0, 0.5);
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0,0.0);
-        constexpr auto e = easing::math::pow(2.0, 0.0);
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0,-0.5);
-        constexpr auto e = easing::math::pow(2.0, -0.5);
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0,-1.0);
-        constexpr auto e = easing::math::pow(2.0, -1.0);
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0,-2.0);
-        constexpr auto e = easing::math::pow(2.0, -2.0);
-        EXPECT_FLOAT_EQ(s,e);
-    }
-    {
-        auto s = std::pow(2.0, -std::numeric_limits<double>::infinity());
-        constexpr auto e = easing::math::pow(2.0, -std::numeric_limits<double>::infinity());
-        EXPECT_FLOAT_EQ(s,e);
-    }
+    // floating point number
+    test_pow<float>();
+    test_pow<double>();
+    test_pow<long double>();
 }
 
+// -------------------------------------
 // sin/cos
 TEST(own_math, sincos)
 {
-    for(int i = 0;i < 360; i += 5)
+    for(int i = 0;i < 360; ++i)
     {
-        auto rad = deg2rad((float)i);
-        //EXPECT_FLOAT_EQ(std::sin(rad), goblib::easing::math::sin(rad)) << i << " | " << rad;
-        EXPECT_TRUE(match_fp(std::sin(rad), easing::math::sin(rad)))
-                << i << " | " << rad << " : " << std::sin(rad) << " / " << easing::math::sin(rad);
-        EXPECT_TRUE(match_fp(std::cos(rad), easing::math::cos(rad)))
-                << i << " | " << rad << " : " << std::cos(rad) << " / " << easing::math::cos(rad);
+        {
+            float f = deg2rad((float)i);
+            EXPECT_TRUE(fp_eq(std::sin(f), easing::math::sin(f)))
+                    << i << " | " << f << " : " << std::sin(f) << " / " << easing::math::sin(f);
+            EXPECT_TRUE(fp_eq(std::cos(f), easing::math::cos(f)))
+                    << i << " | " << f << " : " << std::cos(f) << " / " << easing::math::cos(f);
+        }
+        {
+            double d = deg2rad((double)i);
+            EXPECT_TRUE(fp_eq(std::sin(d), easing::math::sin(d)))
+                    << i << " | " << d << " : " << std::sin(d) << " / " << easing::math::sin(d);
+            EXPECT_TRUE(fp_eq(std::cos(d), easing::math::cos(d)))
+                    << i << " | " << d << " : " << std::cos(d) << " / " << easing::math::cos(d);
+        }
+        {
+            long double ld = deg2rad((long double)i);
+            EXPECT_TRUE(fp_eq(std::sin(ld), easing::math::sin(ld)))
+                    << i << " | " << ld << " : " << std::sin(ld) << " / " << easing::math::sin(ld);
+            EXPECT_TRUE(fp_eq(std::cos(ld), easing::math::cos(ld)))
+                    << i << " | " << ld << " : " << std::cos(ld) << " / " << easing::math::cos(ld);
+        }
     }
 }
 #endif
 
+// -------------------------------------
+// easing
 namespace
 {
 using ease_function_float = float(*)(const float);
@@ -331,8 +352,10 @@ TEST(easing, basic)
             bool b = std::is_same<float, decltype(v) >::value;
 
             EXPECT_TRUE(b) << name[idx];
-            EXPECT_NEAR(float{0}, func(float{0}), FLT_EPSILON) << name[idx];
-            EXPECT_NEAR(float{1}, func(float{1}), FLT_EPSILON) << name[idx];
+            EXPECT_NEAR(0.0f, func(0.0f), FLT_EPSILON) << name[idx];
+            EXPECT_NEAR(1.0f, func(1.0f), FLT_EPSILON) << name[idx];
+            //EXPECT_FLOAT_EQ(float{0}, func(float{0})) << name[idx];
+            //EXPECT_FLOAT_EQ(float{1}, func(float{1})) << name[idx];
 
             for(int32_t i = 0; i < iteration_max; ++i)
             {
@@ -351,9 +374,10 @@ TEST(easing, basic)
             bool b = std::is_same<double, decltype(v) >::value;
 
             EXPECT_TRUE(b) << name[idx];
-            EXPECT_NEAR(double{0}, func(double{0}), DBL_EPSILON) << name[idx];
-            EXPECT_NEAR(double{1}, func(double{1}), DBL_EPSILON) << name[idx];
-
+            EXPECT_NEAR(0.0, func(0.0), DBL_EPSILON) << name[idx];
+            EXPECT_NEAR(1.0, func(1.0), DBL_EPSILON) << name[idx];
+            //EXPECT_DOUBLE_EQ(double{0}, func(double{0})) << name[idx];
+            //EXPECT_DOUBLE_EQ(double{1}, func(double{1})) << name[idx];
             for(int32_t i = 0; i < iteration_max; ++i)
             {
                 double t = (double)i / iteration_max;
@@ -369,11 +393,15 @@ TEST(easing, basic)
         {
             auto v = func(0.0L);
             bool b = std::is_same<long double, decltype(v) >::value;
-
             EXPECT_TRUE(b) << name[idx];
+#if defined(GOBLIB_EASING_USING_OWN_MATH)
+            EXPECT_DOUBLE_EQ(0.0L, func(0.0L)) << name[idx];
+            EXPECT_DOUBLE_EQ(1.0L, func(1.0L)) << name[idx];
+#else
             EXPECT_NEAR(0.0L, func(0.0L), LDBL_EPSILON) << name[idx];
             EXPECT_NEAR(1.0L, func(1.0L), LDBL_EPSILON) << name[idx];
-
+#endif
+            
             for(int32_t i = 0; i < iteration_max; ++i)
             {
                 long double t = (long double)i / iteration_max;
